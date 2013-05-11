8afa7c384e4c5add78563b1c3c376aaacc7b969e
Nicola Greco <notsecurity@gmail.com>
2013-05-11 21:44:06 +0200

    Hello hh

---
+++ Readme.md
@@ -0,0 +1,16 @@
+
+# hh
+
+  It makes a javascript function an high order function using hh(&quot;callback&quot;, args)
+
+## Installation
+
+    $ component install nicolagreco/hh
+
+## API
+
+   
+
+## License
+
+  MIT
---
+++ hh.js
@@ -0,0 +1,26 @@
+/**
+ * Gets a function and optional arguments and returns an high order function
+ * You can use it to return function passing parameters when writing callbacks
+ *
+ *    function test(item) {
+ *      console.log(item);
+ *    }
+ *
+ *    hh(test, "this is a test");
+ *    // will return a function
+ *
+ *
+ * @param {Object} obj
+ * @param {Element} el
+ * @api public
+ */
+
+function hh(func) {
+  var args = Array.prototype.slice.call(arguments).splice(1);
+  return function() {
+    var allArguments = args.concat(Array.prototype.slice.call(arguments));
+    return func.apply(this, allArguments);
+  };
+}
+
+module.exports = hh;
